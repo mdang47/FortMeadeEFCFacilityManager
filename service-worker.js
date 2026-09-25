6dcf5cc6340540a8165b8fa803ebcd26e9819658
mdang47 <113074420+mdang47@users.noreply.github.com>
2026-09-25 13:48:58 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,45 @@
-const CACHE_NAME = "efc-tools-v1";
+const CACHE_NAME = "efc-tools-v2";
 
 const CORE_FILES = [
+  // Main EFC landing page
   "./",
   "./index.html",
   "./manifest.json",
-  "./Facility%20Manager/Apps/Icons/EFC%20Brandmark.png"
+
+  // Main branding
+  "./Facility%20Manager/Apps/Icons/EFC%20Brandmark.png",
+  "./Facility%20Manager/Apps/Icons/Eagle%20Logo.png",
+
+  // App icons
+  "./Facility%20Manager/Apps/Icons/Inventory.png",
+  "./Facility%20Manager/Apps/Icons/Leave.png",
+  "./Facility%20Manager/Apps/Icons/Visitor.png",
+
+  // FAC
+  "./Facility%20Manager/FAC/index.html",
+  "./Facility%20Manager/FAC/pfra_calculator.html",
+
+  // Fitness Access
+  "./Facility%20Manager/Apps/Fitness%20Access/fitness-access.html",
+  "./Facility%20Manager/Apps/Fitness%20Access/review.html",
+  "./Facility%20Manager/Apps/Fitness%20Access/signature.html",
+  "./Facility%20Manager/Apps/Fitness%20Access/statement.html",
+
+  // Inventory
+  "./Facility%20Manager/Apps/Inventory/inventory.html",
+
+  // Leave Tracker
+  "./Facility%20Manager/Apps/Leave%20Tracker/Leave.html",
+
+  // Visitor Counter
+  "./Facility%20Manager/Apps/Visitor%20Counter/index.html"
 ];
 
-// Install
+
+// -------------------------
+// INSTALL
+// -------------------------
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -18,36 +50,67 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-// Activate
+
+// -------------------------
+// ACTIVATE
+// -------------------------
+
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+      return Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      )
-    )
+      );
+    })
   );
 
   self.clients.claim();
 });
 
-// Fetch
+
+// -------------------------
+// FETCH
+// -------------------------
+
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+
+  // Only cache GET requests
+  if (event.request.method !== "GET") {
+    return;
+  }
 
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
 
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
+    fetch(event.request)
+
+      .then(response => {
+
+        // Only cache successful same-origin responses
+        if (
+          response &&
+          response.status === 200 &&
+          response.type === "basic"
+        ) {
+
+          const responseCopy = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseCopy);
+          });
+
+        }
 
         return response;
+
       })
-      .catch(() => caches.match(event.request))
+
+      // If internet isn't available, use cached version
+      .catch(() => {
+        return caches.match(event.request);
+      })
+
   );
+
 });
