b2965628983c1197ff85931911f6c973bba46a24
mdang47 <113074420+mdang47@users.noreply.github.com>
2026-09-25 13:56:59 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "efc-tools-v2";
+const CACHE_NAME = "efc-tools-v3";
 
 const CORE_FILES = [
   // Main EFC landing page
@@ -32,7 +32,7 @@ const CORE_FILES = [
   "./Facility%20Manager/Apps/Leave%20Tracker/Leave.html",
 
   // Visitor Counter
-  "./Facility%20Manager/Apps/Visitor%20Counter/index.html"
+
 ];
 
 
